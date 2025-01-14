22e4cb5b5a15c701513486b007f2fb999e82452e
Jiahua Jiang <jiahua212@gmail.com>
2025-01-14 00:21:53 -0800

    addted the text amd radio

--- PizzaParty/app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ PizzaParty/app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -4,25 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zybooks.pizzaparty.ui.theme.PizzaPartyTheme
+import kotlin.math.ceil
 
 class MainActivity : ComponentActivity() {
+    fun calculateNumPizzas(
+        numPeople: Int,
+        hungerLevel: String
+    ): Int {
+        val slicesPerPizza = 8
+        val slicesPerPerson = when (hungerLevel) {
+            "Light" -> 2
+            "Medium" -> 3
+            else -> 4
+        }
+
+        return ceil(numPeople * slicesPerPerson / slicesPerPizza.toDouble()).toInt()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -52,7 +74,9 @@ fun PizzaPartyScreen(modifier: Modifier = Modifier) {
         )
         NumberField(
             labelText = "Number of people?",
-            modifier = modifier.padding(bottom = 16.dp).fillMaxWidth()
+            modifier = modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth()
         )
         RadioGroup(
             labelText = "How hungry?",
@@ -81,7 +105,18 @@ fun NumberField(
     labelText: String,
     modifier: Modifier = Modifier
 ) {
-    // Not implemented yet
+    var textInput by remember { mutableStateOf("") }
+
+    TextField(
+        value = textInput,
+        onValueChange = { textInput = it },
+        label = { Text(labelText) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number
+        ),
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -91,7 +126,32 @@ fun RadioGroup(
     selectedValue: String,
     modifier: Modifier = Modifier
 ) {
-    // Not implemented yet
+    var selectedOption by remember { mutableStateOf(selectedValue) }
+    val isSelectedOption: (String) -> Boolean = { selectedOption == it}
+    Column {
+        Text(labelText)
+        radioOptions.forEach { option ->
+            Row(
+                modifier = modifier.selectable(
+                    selected = isSelectedOption(option),
+                    onClick = { selectedOption = option},
+                    role = Role.RadioButton
+                ).padding(8.dp)
+            ){
+                RadioButton(
+                    selected = isSelectedOption(option),
+                    onClick = null,
+                    modifier = modifier.padding(end = 8.dp)
+                )
+                Text(
+                    text = option,
+                    modifier = modifier.fillMaxWidth()
+                )
+
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
