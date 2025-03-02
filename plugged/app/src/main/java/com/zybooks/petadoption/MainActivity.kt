e0ed54948d8d06657de6d15903a6c2a76d372569
Jiahua Jiang <jiahua212@gmail.com>
2025-03-01 21:39:36 -0800

    start separations

--- plugged/app/src/main/java/com/zybooks/petadoption/MainActivity.kt
+++ plugged/app/src/main/java/com/zybooks/petadoption/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.zybooks.petadoption
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,38 +9,13 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 import com.zybooks.petadoption.ui.PluggedApp
 import com.zybooks.petadoption.ui.theme.PetAdoptionTheme
-
-import android.Manifest
-import android.content.pm.PackageManager
-
-import android.util.Log
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.java_websocket.WebSocket
-import org.java_websocket.client.WebSocketClient
-import org.java_websocket.handshake.ClientHandshake
-import org.java_websocket.handshake.ServerHandshake
-import org.java_websocket.server.WebSocketServer
-import java.net.InetSocketAddress
 import java.net.NetworkInterface
-import java.net.URI
-import java.net.URISyntaxException
-import java.nio.ByteBuffer
+
 class MainActivity : ComponentActivity() {
    private val logMessages = mutableStateListOf<String>()
    private val requestPermissionsLauncher = registerForActivityResult(
@@ -50,15 +27,60 @@ class MainActivity : ComponentActivity() {
 
       }
    }
+
+   private fun requestNeededPermissions() {
+      val permissions = arrayOf(
+         Manifest.permission.INTERNET,
+         Manifest.permission.ACCESS_NETWORK_STATE,
+         Manifest.permission.ACCESS_WIFI_STATE
+      )
+
+      val allGranted = permissions.all {
+         ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+      }
+
+      if (!allGranted) {
+         requestPermissionsLauncher.launch(permissions)
+      }
+   }
+
+   private fun getLocalIpAddress(): String {
+      try {
+         val networkInterfaces = NetworkInterface.getNetworkInterfaces().toList()
+
+         for (networkInterface in networkInterfaces) {
+            if (networkInterface.isLoopback || !networkInterface.isUp) {
+               continue
+            }
+
+            val addresses = networkInterface.inetAddresses.toList()
+            for (address in addresses) {
+               if (address.isLoopbackAddress || address.hostAddress.contains(":")) {
+                  continue
+               }
+
+               return address.hostAddress
+            }
+         }
+      } catch (e: Exception) {
+         e.printStackTrace()
+      }
+
+      return "Unknown"
+   }
+
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
+
+      requestNeededPermissions()
+      val ipAddress = getLocalIpAddress()
       setContent {
          PetAdoptionTheme {
             Surface(
                modifier = Modifier.fillMaxSize(),
                color = MaterialTheme.colorScheme.background
             ) {
-               PluggedApp()
+               PluggedApp(ipAddress)
             }
          }
       }
--- plugged/app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
+++ plugged/app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
@@ -1,75 +1,52 @@
 package com.zybooks.petadoption.ui
 
-import android.content.Context
-import android.content.Intent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
-import com.zybooks.petadoption.data.Pet
-import com.zybooks.petadoption.data.PetDataSource
-import com.zybooks.petadoption.data.PetGender
-import com.zybooks.petadoption.ui.theme.PetAdoptionTheme
 import kotlinx.serialization.Serializable
-import androidx.compose.foundation.text.KeyboardOptions
-
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Person
-
-import androidx.compose.ui.graphics.Color
-
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.TextField
-
 
 
 sealed class Routes {
@@ -95,9 +72,9 @@ sealed class Routes {
 
 
 @Composable
-fun PluggedApp() {
+fun PluggedApp(ipAddress: String) {
    val navController = rememberNavController()
-
+   val TAG = "Socketty"
    NavHost(
       navController = navController,
       startDestination = Routes.Start
@@ -151,46 +128,10 @@ fun PluggedApp() {
 
 
       }
-
-      composable<Routes.Adopt> { backstackEntry ->
-         val adopt: Routes.Adopt = backstackEntry.toRoute()
-
-         AdoptScreen(
-            petId = adopt.petId,
-            onUpClick = {
-               navController.navigateUp()
-            }
-         )
-      }
    }
 }
 
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun PetAppBar(
-   title: String,
-   modifier: Modifier = Modifier,
-   canNavigateBack: Boolean = false,
-   onUpClick: () -> Unit = { },
-
-   ) {
-   TopAppBar(
-      title = { Text(title) },
-      colors = TopAppBarDefaults.topAppBarColors(
-         containerColor = MaterialTheme.colorScheme.primaryContainer
-      ),
-      modifier = modifier,
-      navigationIcon = {
-         if (canNavigateBack) {
-            IconButton(onClick = onUpClick) {
-               Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
-            }
-         }
-      }
-
-   )
-}
+//Socket functionality
 
 @Composable
 fun InteractScreen(
@@ -490,50 +431,7 @@ fun StartScreen(
    }
 }
 
-@Composable
-fun TeacherPrompt(serverId: String, onRefresh:()->Unit){
 
-   ItemWithCaption(
-      caption="Session Id",
-      item = {
-         Row( verticalAlignment = Alignment.CenterVertically ) {
-            Text(serverId)
-            RefreshButton(onRefresh = onRefresh)
-         }
-      }
-   )
-
-   Button(
-      onClick = {},
-      colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
-   ) {
-      Text("Start!")
-   }
-//   RedoRoleButton()
-}
-
-@Composable
-fun StudentPrompt(textInput: String, onValueChange: (String)-> Unit,){
-   ItemWithCaption(
-      caption = "Class Code",
-      item = {
-         TextField(
-            value = textInput,
-            onValueChange = onValueChange,
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(
-               keyboardType = KeyboardType.Number),
-         )
-      }
-   )
-   Button(
-      onClick = {},
-      colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
-   ) {
-      Text("Connect!")
-   }
-
-}
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -600,191 +498,8 @@ fun PluggedBottomBar(){
 
    }
 }
-@Composable
-fun ListScreen(
-   viewModel: ListViewModel = viewModel(),
-   onImageClick: (Pet) -> Unit,
-   modifier: Modifier = Modifier
-) {
-   Scaffold(
-      topBar = {
-         PetAppBar(
-            title = "Find a Friend"
-         )
-      }
-   ) { innerPadding ->
-      LazyVerticalGrid(
-         columns = GridCells.Adaptive(minSize = 128.dp),
-         contentPadding = PaddingValues(0.dp),
-         modifier = modifier.padding(innerPadding)
-      ) {
-         items(viewModel.petList) { pet ->
-            Image(
-               painter = painterResource(id = pet.imageId),
-               contentDescription = "${pet.type} ${pet.gender}",
-               modifier = Modifier.clickable(
-                  onClick = { onImageClick(pet) },
-                  onClickLabel = "Select the pet"
-               )
-            )
-         }
-      }
-   }
-}
 
-@Preview
-@Composable
-fun PreviewListScreen() {
-   PetAdoptionTheme {
-      ListScreen(
-//         petList = PetDataSource().loadPets(),
-         onImageClick = { }
-      )
-   }
-}
 
-@Composable
-fun DetailScreen(
-   viewModel: DetailViewModel = viewModel(),
-   onAdoptClick: () -> Unit,
-   modifier: Modifier = Modifier,
-   onUpClick: () -> Unit = { },
-   petId: Int,
-) {
-   val pet = viewModel.getPet(petId)
-   val gender = if (pet.gender === PetGender.MALE) "Male" else "Female"
 
-   Scaffold(
-      topBar = {
-         PetAppBar(
-            title = "Details",
-            canNavigateBack = true,
-            onUpClick = onUpClick
 
-         )
-      }
-   ) { innerPadding ->
-      Column(
-         modifier = modifier.padding(innerPadding)
-      ) {
-         Image(
-            painter = painterResource(pet.imageId),
-            contentDescription = pet.name,
-            contentScale = ContentScale.FillWidth
-         )
-         Column(
-            verticalArrangement = Arrangement.spacedBy(6.dp),
-            modifier = modifier.padding(6.dp)
-         ) {
-            Row(
-               horizontalArrangement = Arrangement.SpaceBetween,
-               verticalAlignment = Alignment.CenterVertically,
-               modifier = modifier.fillMaxWidth()
-            ) {
-               Text(
-                  text = pet.name,
-                  style = MaterialTheme.typography.headlineMedium
-               )
-               Button(onClick = onAdoptClick) {
-                  Text("Adopt Me!")
-               }
-            }
-            Text(
-               text = "Gender: $gender",
-               style = MaterialTheme.typography.bodyLarge
-            )
-            Text(
-               text = "Age: ${pet.age}",
-               style = MaterialTheme.typography.bodyLarge
-            )
-            Text(
-               text = pet.description,
-               style = MaterialTheme.typography.bodyMedium
-            )
-         }
-      }
-   }
-}
 
-@Preview
-@Composable
-fun PreviewDetailScreen() {
-   val pet = PetDataSource().loadPets()[0]
-   PetAdoptionTheme {
-      DetailScreen(
-         petId = pet.id,
-         onAdoptClick = { }
-      )
-   }
-}
-
-@Composable
-fun AdoptScreen(
-   petId: Int,
-   modifier: Modifier = Modifier,
-   onUpClick: () -> Unit = { },
-   viewModel: AdoptViewModel = viewModel(),
-) {
-   val pet = viewModel.getPet(petId)
-   val context = LocalContext.current
-   Scaffold(
-      topBar = {
-         PetAppBar(
-            title = "Thank You!",
-            canNavigateBack = true,
-            onUpClick = onUpClick
-
-         )
-      }
-   ) { innerPadding ->
-      Column(
-         modifier = modifier.padding(innerPadding)
-      ) {
-         Row(verticalAlignment = Alignment.CenterVertically) {
-            Image(
-               painter = painterResource(pet.imageId),
-               contentDescription = pet.name,
-               modifier = modifier.size(150.dp)
-            )
-            Text(
-               text = "Thank you for adopting ${pet.name}!",
-               modifier = modifier.padding(horizontal = 28.dp),
-               textAlign = TextAlign.Center,
-               style = MaterialTheme.typography.headlineLarge,
-            )
-         }
-         Text(
-            text = "Please pick up your new family member during business hours.",
-            modifier = modifier.padding(6.dp),
-         )
-         Button(
-            onClick = { shareAdoption(context, pet) },
-            modifier = modifier.padding(6.dp)
-         ) {
-            Icon(Icons.Default.Share, null)
-            Text("Share", modifier = modifier.padding(start = 8.dp))
-         }
-      }
-   }
-}
-
-@Preview
-@Composable
-fun PreviewAdoptScreen() {
-   val pet = PetDataSource().loadPets()[0]
-   PetAdoptionTheme {
-      AdoptScreen(pet.id)
-   }
-}
-
-fun shareAdoption(context: Context, pet: Pet) {
-   val intent = Intent(Intent.ACTION_SEND).apply {
-      type = "text/plain"
-      putExtra(Intent.EXTRA_SUBJECT, "Meet ${pet.name}!")
-      putExtra(Intent.EXTRA_TEXT, "I've adopted ${pet.name}!")
-   }
-
-   context.startActivity(
-      Intent.createChooser(intent, "Pet Adoption")
-   )
-}
